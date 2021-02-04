ac3483476112617be6eacf05876df2db1928f69d
Alex Reyes <reyesruizalex@gmail.com>
2021-02-03 21:34:18 -0500

    All done

--- twitterBot.py
+++ twitterBot.py
@@ -1,9 +1,4 @@
-# todo: tweet every time it goes up 1%
-# congrats message
-		# summary message  
-
 from datetime import date
-import sys
 import os
 import tweepy
 from dotenv import load_dotenv
@@ -14,16 +9,17 @@ TWITTER_API_SECRET = os.getenv('TWITTER_API_SECRET')
 TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
-def send_tweet(tweet):
-	auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-	auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-	api = tweepy.API(auth)
 
-	api.update_status(tweet)
+def send_tweet(tweet):
+    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    api.update_status(tweet)
 
 
 # shoutout: https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
-def progress(count, total):
+def progressBar(count, total):
     bar_len = 50
     filled_len = int(round(bar_len * count / float(total)))
 
@@ -31,38 +27,55 @@ def progress(count, total):
     bar = '|' * filled_len + '-' * (bar_len - filled_len)
     return ('[%s] %s%s \r' % (bar, percents, '%'))
 
+
+def summary():
+    firstDay = date(2017, 9, 5)
+    lastDay = date(2021, 4, 29)
+    birth = date(1999, 9, 28)
+    today = date.today()
+
+    daysAlive = today - birth
+    totalDaysOfUndergrad = lastDay - firstDay
+    daysSinceStart = today - firstDay
+
+    progress = progressBar(calculateProgress(), 100)
+    percentSchool = float((daysSinceStart.days/daysAlive.days)*100)
+    summaryTweet = "Now that @alexreyes243 is done with college, here's a summary: \n\nTotal days being alive: %d\nTotal days of undergrad: %d\nTotal %s of life in undergrad: %1.f%s 😱\n\nTotal times he considered dropping out: ∞\n\n%s" % (daysAlive.days, totalDaysOfUndergrad.days, "%", percentSchool, "%", progress)
+
+    return summaryTweet
+
+
 def calculateProgress():
-	firstDay = date(2017, 9, 5)
-	lastDay = date(2021, 4, 29)
-	birth = date(1999, 9, 28)
-	today = date.today() 
+    firstDay = date(2017, 9, 5)
+    lastDay = date(2021, 4, 29)
+    today = date.today()
 
-	daysAlive = today - birth
-	totalDaysOfUndergrad = lastDay - firstDay
-	daysSinceStart = today - firstDay
-	daysLeft = lastDay - today
+    totalDaysOfUndergrad = lastDay - firstDay
+    daysSinceStart = today - firstDay
 
-	print("Total days  being alive: ", daysAlive.days)
-	print("Total days of undergrad: ", totalDaysOfUndergrad.days)
-	print("Days since school start: ", daysSinceStart.days)
-	print("Days till school finish: ", daysLeft.days)
-	print()
+    percent = float((daysSinceStart.days/totalDaysOfUndergrad.days)*100)
 
-	symbol = " % "
-	percentSchool = float((daysSinceStart.days/daysAlive.days)*100)
-	print("%1.f%s of life spent in undergrad 😱" % (percentSchool, "%"))
+    return percent
 
-	print()
-	# print("Undergrad progress: ", end='')
-	percent = float((daysSinceStart.days/totalDaysOfUndergrad.days)*100)
 
-	return percent
+def main():
+    progressPercent = calculateProgress()
+    summaryTweet = summary()
 
-def main(): 
-	percent = calculateProgress()
-	progressBar = progress(percent,100)
+    if (progressPercent == 100):
+        print("Sending final 100% tweet!")
+        send_tweet("@alexreyes243 is officially a Penn State graduate 🎓 🎉 🥳")
+        send_tweet(summaryTweet)
 
-	print(progressBar)
-	# send_tweet(progressBar)
+    elif (progressPercent > 100):
+        print("Already done with this bot account!")
+        exit()
+
+    else:
+        print("Sending progress tweet")
+        progress = progressBar(progressPercent, 100)
+
+        send_tweet(progress)
+
 
 main()
